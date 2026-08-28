0c7149a85e133c8c5ab7a1b02c8b18cd5599af26
ariyan54555 <mdariyanzaman@gmail.com>
2026-08-28 23:06:23 +0600

    Initial commit

---
+++ First.py
@@ -0,0 +1,79 @@
+print("Computer Science and Engineering")
+print("My name is Ariyan Zaman","My Age is",23)
+print(4+87)
+uni="Daffodil International University"
+Location="Dhaka,Bangladesh"
+price= 434
+print("University Name:",uni)
+print("Location:",Location)
+
+print(type(uni))
+print(type(price))
+
+age= 23
+old = False
+
+a= None
+print(type(age))
+print(type(old))
+print(type(a))
+
+g=21
+j=687
+sum=g+j
+print("Sum=",sum)
+#Hello Boss this is my last python code
+"""
+Heloo boss this is nothing for me. I am Originally from Bangladesh. I am a student of Daffodil International University. I am studying in the department of Computer Science and Engineering. I am 23 years old. I have a lot of interest in programming. I want to be a good programmer in the future. I will try my best to achieve my goal. Thank you for your support.
+
+"""
+#Arithmatic operators
+a= 5
+b= 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)#always return float value
+print(a//b)#always return integer value
+print(a%b)
+print(a**b)# returns a^b
+#Relational Operators
+y=0
+u=78
+print(y==u)#False
+print(y!=u)#True
+#Assignment Operators
+x= 5
+x+= 3# equivalent to x= x+3
+print(x)
+
+#Logical Operators
+p=True
+q=False
+print(not p)
+print(p and q)
+print(p or q)
+
+#Type conversion
+data="43"
+print(type(data))
+data=int(data)
+print(type(data))
+#Input
+name=input("Enter Your name:")#input function always return string value
+print("Hello",name)
+val=input("Enter Your CGPA: ")
+print(type(val),val)
+#Type casting in input
+value=int(input("Age: "))
+print(type(value),value)
+
+#Practice
+""""
+write a program to take two numbers and print ther sum
+
+"""
+h=int(input("Enter first num: "))
+k=int(input("Enter second num: "))
+sum=h+k
+print("Sum = ",sum)
