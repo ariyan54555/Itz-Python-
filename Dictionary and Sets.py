797fd0b4361d6c68479f11f4e93eeda6b3c15bd4
ariyan54555 <mdariyanzaman@gmail.com>
2026-08-31 21:45:32 +0600

    commit

---
+++ Dictionary and Sets.py
@@ -0,0 +1,21 @@
+#Dictionary
+# It is used to store data in key:value pairs
+# It is Unordered, mutable and don't allow duplicate keys
+# Have no index
+
+Apurba_Info={
+    "Key" : "Value",
+    "Name" : "Apurba Das Arpan",
+    "Age" : 23,
+    "Id" : "251-15-103",
+    "University CGPA" : 4.00,
+    "Gender" : "Male",
+    "Phone Number" : "01634283759",
+    "List Of GF" : ["Ratri Paul","Brishty Paul","Antara Mondal"],
+
+}
+print(Apurba_Info)
+print(type(Apurba_Info))
+print(Apurba_Info["Age"])
+print(Apurba_Info["Id"])
+print(Apurba_Info["List Of GF"])
