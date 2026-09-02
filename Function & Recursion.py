86b6ee6dec3852654a0e216e8d80a54297bbe314
ariyan54555 <mdariyanzaman@gmail.com>
2026-09-02 22:40:37 +0600

    Last commit

--- Function & Recursion.py
+++ Function & Recursion.py
@@ -17,3 +17,58 @@ def average(a,b,c):
 
 avg = average(3,4,5)
 print("Avg = ",avg)
+
+#WAF to find the length of a list
+def len_City(city):
+    return len(city)
+
+city = ["Dhaka","khulna","Rajshahi","Barishal"]
+
+length = len_City(city)
+print("Length of the list = ",length)
+
+#WAF to print the elements of a list in a single line
+def printlist(city):
+    for el in city:
+        print(el,end=" ")
+
+printlist(city)
+print()
+#WAF to find the factorial of n
+def factorial(n):
+    fact =1
+    for i in range(1,n+1):
+        fact = fact*i
+    return fact
+
+fact = factorial(5)
+print("Factorial of 5 = ",fact)
+
+
+#Recursion
+def show(n):
+    if n==0:
+        return
+    print(n)
+    show(n-1)
+
+show(5)
+
+#Write a recursive function to find the sum of n natural numbers
+
+def sum_calc(n):
+    if n==0:
+        return 0
+    return n + sum_calc(n-1)
+
+sum = sum_calc(3)
+print("Sum = ",sum)
+
+#write a recursive function to print the elements of a list
+def printlist_recursion(city,index):
+    if index == len(city):
+        return
+    print(city[index],end=" ")
+    printlist_recursion(city,index+1)
+
+printlist_recursion(city,0)
