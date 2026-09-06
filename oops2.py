5c288547b037627fe14bd100ce0169a886ade22d
ariyan54555 <mdariyanzaman@gmail.com>
2026-09-06 20:36:53 +0600

    excitful commit

---
+++ oops2.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self,name):
+        self.name = name
+
+s1 = student("Ariyan")
+print(s1.name)
+# del s1  --> this is "del" keyword by which we can delete an entire object from the memory
+
+# private attribute and method
+
